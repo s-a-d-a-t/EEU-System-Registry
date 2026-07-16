29d089cda839f53c3f1f6d120594c308307da55c
sadat <sdrkk66@gmail.com>
2026-07-16 12:11:56 +0300

    feat: implement authentication context with login and token management

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,1 +1,59 @@
+import { createContext, useState ,useEffect} from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
+    const [token, setToken] = useState(null);
+
+//login 
+
+const login = (userData, userToken) => {
+    setUser(userData);
+    setToken(userToken);
+
+    //saving the token and user in local storage 
+    localStorage.setItem("token", userToken);
+    localStorage.setItem("user", JSON.stringify(userData));
+};
+
+//logout
+const logout = () => {
+    setUser(null);
+    setToken(null);
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+};
+
+
+//restore login
+useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    const savedUser = localStorage.getItem("user");
+    const userData = savedUser ? JSON.parse(savedUser) : null;
+
+    if(savedToken && userData){
+        setToken(savedToken);
+        setUser(userData);
+    }
+},
+[]);
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                token,
+                login,
+                logout
+            }}
+        >
+
+            {children}
+
+        </AuthContext.Provider>
+    );
+};
 
