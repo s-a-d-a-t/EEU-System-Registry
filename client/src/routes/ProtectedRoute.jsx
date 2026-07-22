2e047868a909509e3f81acf14a6a993d65e583f4
sadat <sdrkk66@gmail.com>
2026-07-22 16:47:08 +0300

    feat: implement ProtectedRoute component for route protection based on authentication token

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,1 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import {useAuth} from "../context/AuthContext";
 
+const ProtectedRoute = () =>{
+    const {token} = useAuth();
+
+    if(!token){
+        return <Navigate to = "/login" replace />;
+    }
+
+    // render protected page
+    return <Outlet />; 
+
+};
+
+export default ProtectedRoute;
